b26e2c104cf32d677280a6e524cb74a351c0b74c
Edmund Li <edmund.xz.lee@gmail.com>
2016-01-17 11:17:18 -0800

    rest spring!

--- gs-consuming-rest-initial/src/main/java/hello/Application.java
+++ gs-consuming-rest-initial/src/main/java/hello/Application.java
@@ -8,18 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 	
-	public static final Logger log = LoggerFactory.getLogger(Application.class);
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	public static void main(String args[]) {
+		SpringApplication.run(Application.class);
 	}
 	
-	public void run(String...strings) throws Exception {
+	@Override
+	public void run(String... strings) throws Exception {
 		RestTemplate restTemplate = new RestTemplate();
 		Quote quote = restTemplate.getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
 		log.info(quote.toString());
 	}
-
 }
